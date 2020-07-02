39f65abaac1c28800a0b642fb0aa0fa0c16f5366
1Mill <erik.ekberg@outlook.com>
2020-07-01 20:31:04 -0700

    Formatting

--- ci-deploy.bash
+++ ci-deploy.bash
@@ -1,7 +1,7 @@
 VERSION=`date +%Y-%m-%d_%H-%M-%S`
 
-echo "${PASSWORD}" | docker login --username ${USERNAME} --password-stdin
-&& docker build . --file Dockerfile --tag tmp-image
-&& docker tag tmp-image 1mill/terraform-sops:${VERSION}
-&& docker push 1mill/terraform-sops:${VERSION}
-&& docker push 1mill/terraform-sops:latest
+echo "${PASSWORD}" | docker login --username ${USERNAME} --password-stdin && \
+docker build . --file Dockerfile --tag tmp-image && \
+docker tag tmp-image 1mill/terraform-sops:${VERSION} && \
+docker push 1mill/terraform-sops:${VERSION} && \
+docker push 1mill/terraform-sops:latest
