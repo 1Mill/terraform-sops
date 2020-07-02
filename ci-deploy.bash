21042182acc924a1aeeed7221fc81586cb2b6104
1Mill <erik.ekberg@outlook.com>
2020-07-01 20:39:41 -0700

    Abstract username and imagename

--- ci-deploy.bash
+++ ci-deploy.bash
@@ -1,8 +1,9 @@
+IMAGENAME=terraform-sops
 VERSION=`date +%Y-%m-%d_%H-%M-%S`
 
 echo "${PASSWORD}" | docker login --username ${USERNAME} --password-stdin && \
-docker build . --file Dockerfile --tag tmp-image && \
-docker tag tmp-image 1mill/terraform-sops:${VERSION} && \
-docker tag tmp-image 1mill/terraform-sops:latest && \
-docker push 1mill/terraform-sops:${VERSION} && \
-docker push 1mill/terraform-sops:latest
+docker build . --file Dockerfile --tag temp-image && \
+docker tag temp-image ${USERNAME}/${IMAGENAME}:${VERSION} && \
+docker tag temp-image ${USERNAME}/${IMAGENAME}:latest && \
+docker push ${USERNAME}/${IMAGENAME}:${VERSION} && \
+docker push ${USERNAME}/${IMAGENAME}:latest
