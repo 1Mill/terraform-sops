bb44091b5eb535f3d606e26c1f4072372fe9d7d3
1Mill <erik.ekberg@outlook.com>
2020-07-05 12:11:43 -0700

    Update workflow

--- ci-deploy.bash
+++
@@ -1,9 +0,0 @@
-IMAGENAME=terraform-sops
-VERSION=`date +%Y-%m-%d_%H-%M-%S`
-
-echo "${PASSWORD}" | docker login --username ${USERNAME} --password-stdin && \
-docker build . --file Dockerfile --tag temp-image && \
-docker tag temp-image ${USERNAME}/${IMAGENAME}:${VERSION} && \
-docker tag temp-image ${USERNAME}/${IMAGENAME}:latest && \
-docker push ${USERNAME}/${IMAGENAME}:${VERSION} && \
-docker push ${USERNAME}/${IMAGENAME}:latest
